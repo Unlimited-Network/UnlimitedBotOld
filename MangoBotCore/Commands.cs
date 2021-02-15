3323ffbe8a565eaacbce8f52febd049875cfab1b
Seth Feuer <lXxMangoxXl@gmail.com>
2021-02-15 18:01:39 -0500

    Removed UnirestAPI (again)

--- MangoBotCore/Commands.cs
+++ MangoBotCore/Commands.cs
@@ -9,7 +9,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using unirest_net.http;
 
 namespace UnlimitedBotCore {
     public class PPSize {
